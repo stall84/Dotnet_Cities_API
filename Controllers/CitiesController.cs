b4023d0066028f13125f8cec5edadec2f7161776
Michael Stallings <stall84@gmail.com>
2020-08-14 15:38:57 -0400

    Initial Commit

---
+++ Controllers/CitiesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        
+        // Reformated using IActionResult instead of JsonResult to be able to provide detailed status code in
+        // response to client (Ok/NotFound)
+        public IActionResult GetCity(int id)
+        {
+            // find the city from our datastore class
+            var cityReturn = CitiesDataStore.Current.Cities
+                .FirstOrDefault(q => q.Id == id);
+            if (cityReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(cityReturn);
+        }
+
+    }
+}
