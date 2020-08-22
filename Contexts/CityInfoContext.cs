024edd80dd8411e36820345e76b7f374e3d4db65
Michael Stallings <stall84@gmail.com>
2020-08-21 23:25:10 -0400

    Completed wiring up and configuration of sql servr database

---
+++ Contexts/CityInfoContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Contexts
+{
+    public class CityInfoContext : DbContext
+    {
+        // DbSet property does the magic of converting requests to SQL database queries, etc
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointsOfInterest { get; set; }
+        // Constructor below
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionString")
+        //}
+    }
+}
